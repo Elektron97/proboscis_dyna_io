ee6e53a3e00d670e7f32dcacef365b3b7ebe01f1
Elektron97 <danele.caradonna@gmail.com>
2023-06-18 17:48:11 +0200

    removing pid

--- proboscis_pkg/src/control/control_utils.cpp
+++ proboscis_pkg/src/control/control_utils.cpp
@@ -9,7 +9,7 @@ Control_Node::Control_Node()
 {
     // Init vector objs
     turn_commands.data = vector<float>(N_MOTORS);   // store command
-    ref_currents.data = vector<float>(N_MOTORS);    // store ref
+    //ref_currents.data = vector<float>(N_MOTORS);    // store ref
 
     // Init iterator
     //i = 0;
@@ -24,21 +24,22 @@ Control_Node::~Control_Node()
 void Control_Node::joy_callBack(const sensor_msgs::Joy::ConstPtr& msg)
 {
     // --- Read Joystick and map into turn commands --- //
-    //joy2Motors(*msg, turn_commands, MAX_OUTPUT);  // [Old] -> just set position
-    joy2Motors(*msg, ref_currents, 0.2);    // for now: setting to 0.2 A all active motors
+    joy2Motors(*msg, turn_commands, MAX_OUTPUT);  // [Old] -> just set position
+    //joy2Motors(*msg, ref_currents, 0.2);    // for now: setting to 0.2 A all active motors
 }
 
+// --- CURRENTLY NOT USED --- //
 void Control_Node::current_callBack(const std_msgs::Float32MultiArray::ConstPtr& msg)
 {
     // --- Read Current and Implement PID Controller --- //
-    for(int i = 0; i < N_MOTORS; i++)
+    /*for(int i = 0; i < N_MOTORS; i++)
     {
         if(i + 1 != 5)
             turn_commands.data[i] = pid_obj.calculate(ref_currents.data[i], msg->data[i]);      // Fixing dt and gains for every motors
         else
             continue;
         //ROS_INFO("PID Control Law for Motor %d | turn = %f", i+1, turn_commands.data[i]);
-    }
+    }*/
 
     // Publish only when a new PID command is computed 
     publish_turns();
@@ -52,8 +53,7 @@ void Control_Node::publish_turns()
 void Control_Node::main_loop(const ros::TimerEvent& event)
 {
     // Publish /cmd_turns
-    //publish_turns();
-    // Maybe it's better to publish only when a new PID command is computed
+    publish_turns();
 }
 
 // --- FUNCTIONS --- //
